7021bc2e53bcc36981b9fdc4b9ac14611ab13f2c
Joseph Taiwo <104542152+Teejay128@users.noreply.github.com>
2024-04-05 22:56:14 +0100

    added basic handlbar views

--- index.js
+++ index.js
@@ -1,35 +1,55 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const taskRouter = require("./routes/taskRouter");
-const userRouter = require("./routes/userRouter");
-const dbConnect = require("./utils/database");
-const port = process.env.PORT || 3000;
+const path = require("path");
+const { engine } = require("express-handlebars");
 require("dotenv").config();
 
+const taskRouter = require("./routes/taskRouter");
+const userRouter = require("./routes/userRouter");
+
+const dbConnect = require("./utils/database");
+
+const port = process.env.PORT || 3000;
 const app = express();
+
+app.engine(
+	"hbs",
+	engine({
+		layoutsDir: `${__dirname}/views/layouts`,
+		extname: "hbs",
+		defaultLayout: "index",
+		partialsDir: `${__dirname}/views/partials`,
+	})
+);
+app.set("view engine", "hbs");
+app.set("views", "./views");
 
 // Middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 app.use(cookieParser(process.env.SECRET));
 
 app.use("/api/tasks", taskRouter);
 app.use("/api/user", userRouter);
 
-app.use((err, req, res, next) => {
-	console.log(err.stack);
-	res.status(500).send("Internal Server Error");
-});
+// app.use((err, req, res, next) => {
+// 	console.log(err.stack);
+// 	res.status(500).send("Internal Server Error");
+// });
+
 app.get("/", (req, res) => {
-	res.send("Welcome to your web application!");
+	const title = "TaskBloc";
+	const users = ["David", "Joseph", "Grace", "Damola"];
+	res.render("home");
 });
 
 app.get("/login", (req, res) => {
-	res.send("This is the login page");
+	res.render("login");
 });
 
 app.get("/signup", (req, res) => {
-	res.send("This is the signup page");
+	res.render("signup");
 });
 
 // module.exports = app;
